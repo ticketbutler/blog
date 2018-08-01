51e18df52cac5a46da61ff2729a4da9423701bf4
Super <super@Supers-MacBook-Pro.local>
2018-08-01 11:18:19 +0200

    styles and so on

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import './index.css'
+import "../layout-overide.css";
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+const IndexPage = () => ( <
+  div >
+
+  <
+  h1 > Hi people < /h1>  <
+  p > Welcome to your new Gatsby site. < /p>  <
+  p > Now go build something great. < /p>  <
+  Link to = "/page-2/" > Go to page 2 < /Link>  <
+  /div >
 )
 
 export default IndexPage
